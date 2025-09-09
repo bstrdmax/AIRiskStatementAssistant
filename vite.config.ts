5cf8ed403cba03a4c5698f1e818c9bf5b805d173
Jason Maxwell <max371979@gmail.com>
2025-09-09 09:50:16 -0400

    Update vite.config.ts
    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This makes the environment variable available in your client-side code
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  }
 })
