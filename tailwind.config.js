c8a8583203cb660631d9b80c2db41cdbee35ca91
Jason Maxwell <max371979@gmail.com>
2025-09-09 09:50:05 -0400

    Update tailwind.config.js
    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
